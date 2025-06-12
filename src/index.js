423017cbda913b9ccaad3ab2a07b0fcf63c38ff7
Ruhani Mittal <ruhanimttl@gmail.com>
2025-06-12 14:30:37 +0530

    main

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Projects from './components/Projects/projects';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
