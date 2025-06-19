3e26224a8840f9de28257acfdc594c8eede16290
J0ELh <99513652+J0ELh@users.noreply.github.com>
2025-06-18 22:38:20 -0700

    minor changes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename='/website'>
+    <Router>
       <Routes>
         <Route exact path='/' element={<App customComponent={Home} />} />
         <Route exact path='/about' element={<App customComponent={About} />} />
